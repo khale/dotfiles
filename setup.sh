a21e040253664d08ac4638f651ffd05e911eecfb
Kyle Hale <khale@cs.iit.edu>
2020-05-29 10:10:37 -0500

    migrate over fish config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo -n "Installing fish configuration..."
+mkdir -p ~/.config/fish
+cp fish-config ~/.config/fish/config.fish
+echo -n "done"
+
 echo -n "Installing GitNow configuration..."
 cp gitnow-config ~/.gitflow
 echo "done"
+
